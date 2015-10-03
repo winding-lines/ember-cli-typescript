757ba96c7aef3b67e8df5aaa459d41d7f3c3a0b7
Philip Bjorge <philipbjorge@gmail.com>
2015-10-03 12:29:04 -0700

    Fix component

--- blueprints/component/index.js
+++ blueprints/component/index.js
@@ -1,13 +1,3 @@
-/*jshint node:true*/
-
-var stringUtil         = require('ember-cli-string-utils');
-var pathUtil           = require('ember-cli-path-utils');
-var validComponentName = require('../../lib/utilities/valid-component-name');
-var getPathOption      = require('../../lib/utilities/get-component-path-option');
-var path               = require('path');
-
-var normalizeEntityName = require('ember-cli-normalize-entity-name');
-
 module.exports = {
   description: 'Generates a component. Name must contain a hyphen.',
 
@@ -23,54 +13,17 @@ module.exports = {
   ],
 
   fileMapTokens: function() {
-    return {
-      __path__: function(options) {
-        if (options.pod) {
-          return path.join(options.podPath, options.locals.path, options.dasherizedModuleName);
-        }
-        return 'components';
-      },
-      __templatepath__: function(options) {
-        if (options.pod) {
-          return path.join(options.podPath, options.locals.path, options.dasherizedModuleName);
-        }
-        return 'templates/components';
-      },
-      __templatename__: function(options) {
-        if (options.pod) {
-          return 'template';
-        }
-        return options.dasherizedModuleName;
-      }
-    };
+    var blueprint = this.lookupBlueprint('component');
+    return blueprint.fileMapTokens.apply(blueprint, arguments);
   },
 
-  normalizeEntityName: function(entityName) {
-    entityName = normalizeEntityName(entityName);
-
-    return validComponentName(entityName);
+  normalizeEntityName: function() {
+    var blueprint = this.lookupBlueprint('component');
+    return blueprint.normalizeEntityName.apply(blueprint, arguments);
   },
 
-  locals: function(options) {
-    var templatePath   = '';
-    var importTemplate = '';
-    var contents       = '';
-    // if we're in an addon, build import statement
-    if (options.project.isEmberCLIAddon() || options.inRepoAddon && !options.inDummy) {
-      if(options.pod) {
-        templatePath   = './template';
-      } else {
-        templatePath   = pathUtil.getRelativeParentPath(options.entity.name) +
-          'templates/components/' + stringUtil.dasherize(options.entity.name);
-      }
-      importTemplate   = 'import layout from \'' + templatePath + '\';\n';
-      contents         = '\n  layout: layout';
-    }
-
-    return {
-      importTemplate: importTemplate,
-      contents: contents,
-      path: getPathOption(options)
-    };
+  locals: function() {
+    var blueprint = this.lookupBlueprint('component');
+    return blueprint.locals.apply(blueprint, arguments);
   }
 };

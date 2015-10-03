ce90c2974cca16e430bec8cde53364698410fc5d
Philip Bjorge <philipbjorge@gmail.com>
2015-10-03 12:36:01 -0700

    Fix helper-test

--- blueprints/helper-test/index.js
+++ blueprints/helper-test/index.js
@@ -1,14 +1,7 @@
-/*jshint node:true*/
-
-var getDependencyDepth = require('ember-cli-get-dependency-depth');
-var testInfo = require('ember-cli-test-info');
-
 module.exports = {
   description: 'Generates a helper unit test.',
-  locals: function(options) {
-    return {
-      friendlyTestName: testInfo.name(options.entity.name, "Unit", "Helper"),
-      dependencyDepth: getDependencyDepth(options.entity.name)
-    };
+  locals: function() {
+    var blueprint = this.lookupBlueprint('helper-test');
+    return blueprint.locals.apply(blueprint, arguments);
   }
 };

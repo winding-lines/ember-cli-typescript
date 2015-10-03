eaf44097d7079a0792efb994943df560fcc40876
Philip Bjorge <philipbjorge@gmail.com>
2015-10-03 15:24:09 -0700

    Revert "fail test"
    This reverts commit 62f410bca4797e6b122c25e059f752596f4a2a54.

--- tests/dummy/app/app.ts
+++ tests/dummy/app/app.ts
@@ -4,7 +4,6 @@ import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
-sdfsdf;
 
 const App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,

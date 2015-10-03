31e02381a2e82667add558cffd330ae3090ca94b
Philip Bjorge <philipbjorge@gmail.com>
2015-10-03 12:45:13 -0700

    Fix transform test

--- blueprints/transform-test/index.js
+++ blueprints/transform-test/index.js
@@ -1,12 +1,7 @@
-/*jshint node:true*/
-
-var testInfo = require('ember-cli-test-info');
-
 module.exports = {
   description: 'Generates a transform unit test.',
-  locals: function(options) {
-    return {
-      friendlyTestDescription: testInfo.description(options.entity.name, "Unit", "Transform")
-    };
-  },
+  locals: function() {
+    var blueprint = this.lookupBlueprint('transform-test');
+    return blueprint.locals.apply(blueprint, arguments);
+  }
 };

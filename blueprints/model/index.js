7dda82c19a2dc6694e282449092b3114963c6462
Philip Bjorge <philipbjorge@gmail.com>
2015-10-03 14:02:42 -0700

    Fix model blueprint

--- blueprints/model/index.js
+++ blueprints/model/index.js
@@ -1,9 +1,3 @@
-/*jshint node:true*/
-
-var inflection  = require('inflection');
-var stringUtils = require('ember-cli-string-utils');
-var EOL         = require('os').EOL;
-
 module.exports = {
   description: 'Generates an ember-data model.',
 
@@ -12,52 +6,8 @@ module.exports = {
     'attr:type'
   ],
 
-  locals: function(options) {
-    var attrs = [];
-    var needs = [];
-    var entityOptions = options.entity.options;
-
-    for (var name in entityOptions) {
-      var type = entityOptions[name] || '';
-      var dasherizedName = stringUtils.dasherize(name);
-      var dasherizedNameSingular = inflection.singularize(dasherizedName);
-      var camelizedName = stringUtils.camelize(name);
-      var dasherizedType = stringUtils.dasherize(type);
-
-      if (/has-many/.test(dasherizedType)) {
-        var camelizedNamePlural = inflection.pluralize(camelizedName);
-        attrs.push(camelizedNamePlural + ': ' + dsAttr(dasherizedName, dasherizedType));
-      } else {
-        attrs.push(camelizedName + ': ' + dsAttr(dasherizedName, dasherizedType));
-      }
-
-      if (/has-many|belongs-to/.test(dasherizedType)) {
-        needs.push("'model:" + dasherizedNameSingular + "'");
-      }
-    }
-
-    attrs = attrs.join(',' + EOL + '  ');
-    needs = '  needs: [' + needs.join(', ') + ']';
-
-    return {
-      attrs: attrs,
-      needs: needs
-    };
+  locals: function() {
+    var blueprint = this.lookupBlueprint('model');
+    return blueprint.locals.apply(blueprint, arguments);
   }
 };
-
-function dsAttr(name, type) {
-  switch (type) {
-  case 'belongs-to':
-    return 'DS.belongsTo(\'' + name + '\')';
-  case 'has-many':
-    var singularizedName = inflection.singularize(name);
-    return 'DS.hasMany(\'' + singularizedName + '\')';
-  case '':
-    //"If you don't specify the type of the attribute, it will be whatever was provided by the server"
-    //http://emberjs.com/guides/models/defining-models/
-    return 'DS.attr()';
-  default:
-    return 'DS.attr(\'' + type + '\')';
-  }
-}

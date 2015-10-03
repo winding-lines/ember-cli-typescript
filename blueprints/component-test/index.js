ffbedf63470ea3396e499b5ccad2d477fb17bdc6
Philip Bjorge <philipbjorge@gmail.com>
2015-10-03 12:30:51 -0700

    Fix component test

--- blueprints/component-test/index.js
+++ blueprints/component-test/index.js
@@ -1,10 +1,3 @@
-/*jshint node:true*/
-
-var path          = require('path');
-var testInfo      = require('ember-cli-test-info');
-var stringUtil    = require('ember-cli-string-utils');
-var getPathOption = require('../../lib/utilities/get-component-path-option');
-
 module.exports = {
   description: 'Generates a component integration or unit test.',
 
@@ -23,37 +16,12 @@ module.exports = {
   ],
 
   fileMapTokens: function() {
-    return {
-      __testType__: function(options) {
-        return options.locals.testType || 'integration';
-      },
-      __path__: function(options) {
-        if (options.pod) {
-          return path.join(options.podPath, options.locals.path, options.dasherizedModuleName);
-        }
-        return 'components';
-      }
-    };
+    var blueprint = this.lookupBlueprint('component-test');
+    return blueprint.fileMapTokens.apply(blueprint, arguments);
   },
+
   locals: function(options) {
-    var dasherizedModuleName = stringUtil.dasherize(options.entity.name);
-    var componentPathName = dasherizedModuleName;
-    var testType = options.testType || "integration";
-    var friendlyTestDescription = testInfo.description(options.entity.name, "Integration", "Component");
-
-    if (options.pod && options.path !== 'components' && options.path !== '') {
-      componentPathName = [options.path, dasherizedModuleName].join('/');
-    }
-
-    if (options.testType === 'unit') {
-      friendlyTestDescription = testInfo.description(options.entity.name, "Unit", "Component");
-    }
-
-    return {
-      path: getPathOption(options),
-      testType: testType,
-      componentPathName: componentPathName,
-      friendlyTestDescription: friendlyTestDescription
-    };
+    var blueprint = this.lookupBlueprint('component-test');
+    return blueprint.locals.apply(blueprint, arguments);
   }
 };

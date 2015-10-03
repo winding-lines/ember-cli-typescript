580f800ad892e20fa407b5439d1b3b8229daa758
Philip Bjorge <philipbjorge@gmail.com>
2015-10-03 14:09:58 -0700

    Fix route

--- blueprints/route/index.js
+++ blueprints/route/index.js
@@ -1,3 +1,10 @@
+/*jshint node:true*/
+
+var fs          = require('fs-extra');
+var path        = require('path');
+var chalk       = require('chalk');
+var EmberRouterGenerator = require('ember-router-generator');
+
 module.exports = {
   description: 'Generates a route and a template, and registers the route with the router.',
 
@@ -35,3 +42,41 @@ module.exports = {
     updateRouter.call(this, 'remove', options);
   }
 };
+
+function updateRouter(action, options) {
+  var entity = options.entity;
+  var actionColorMap = {
+    add: 'green',
+    remove: 'red'
+  };
+  var color = actionColorMap[action] || 'gray';
+
+  if (this.shouldTouchRouter(entity.name, options)) {
+    writeRoute(action, entity.name, options);
+
+    this.ui.writeLine('updating router');
+    this._writeStatusToUI(chalk[color], action + ' route', entity.name);
+  }
+}
+
+function findRouter(options) {
+  var routerPathParts = [options.project.root];
+
+  if (options.dummy && options.project.isEmberCLIAddon()) {
+    routerPathParts = routerPathParts.concat(['tests', 'dummy', 'app', 'router.js']);
+  } else {
+    routerPathParts = routerPathParts.concat(['app', 'router.js']);
+  }
+
+  return routerPathParts;
+}
+
+function writeRoute(action, name, options) {
+  var routerPath = path.join.apply(null, findRouter(options));
+  var source = fs.readFileSync(routerPath, 'utf-8');
+
+  var routes = new EmberRouterGenerator(source);
+  var newRoutes = routes[action](name, options);
+
+  fs.writeFileSync(routerPath, newRoutes.code());
+}

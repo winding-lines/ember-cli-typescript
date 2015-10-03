1ed4b2756eadb752f4f7e7d727ef3e1a09b53462
Philip Bjorge <philipbjorge@gmail.com>
2015-10-03 14:05:20 -0700

    Fix resource blueprint

--- blueprints/resource/index.js
+++ blueprints/resource/index.js
@@ -1,10 +1,3 @@
-/*jshint node:true*/
-
-var Blueprint  = require('../../lib/models/blueprint');
-var Promise    = require('../../lib/ext/promise');
-var merge      = require('lodash/object/merge');
-var inflection = require('inflection');
-
 module.exports = {
   description: 'Generates a model and route.',
 
@@ -17,51 +10,12 @@ module.exports = {
   },
 
   _processBlueprint: function(type, name, options) {
-    var mainBlueprint = Blueprint.lookup(name, {
-      ui: this.ui,
-      analytics: this.analytics,
-      project: this.project
-    });
-
-    return Promise.resolve()
-      .then(function() {
-        return mainBlueprint[type](options);
-      })
-      .then(function() {
-        var testBlueprint = mainBlueprint.lookupBlueprint(name + '-test', {
-          ui: this.ui,
-          analytics: this.analytics,
-          project: this.project,
-          ignoreMissing: true
-        });
-
-        if (!testBlueprint) { return; }
-
-        if (testBlueprint.locals === Blueprint.prototype.locals) {
-          testBlueprint.locals = function(options) {
-            return mainBlueprint.locals(options);
-          };
-        }
-
-        return testBlueprint[type](options);
-      });
+    var blueprint = this.lookupBlueprint('resource');
+    return blueprint._processBlueprint.apply(blueprint, arguments);
   },
 
   _process: function(type, options) {
-    var entityName = options.entity.name;
-
-    var modelOptions = merge({}, options, {
-      entity: {
-        name: entityName ? inflection.singularize(entityName) : ''
-      }
-    });
-
-    var routeOptions = merge({}, options);
-
-    var self = this;
-    return this._processBlueprint(type, 'model', modelOptions)
-              .then(function() {
-                return self._processBlueprint(type, 'route', routeOptions);
-              });
+    var blueprint = this.lookupBlueprint('resource');
+    return blueprint._process.apply(blueprint, arguments);
   }
 };

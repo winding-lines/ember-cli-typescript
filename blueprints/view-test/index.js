2a897f4bc0decedbc73718cedf50a8b252faf89e
Philip Bjorge <philipbjorge@gmail.com>
2015-10-03 12:45:56 -0700

    Fix view test

--- blueprints/view-test/index.js
+++ blueprints/view-test/index.js
@@ -1,12 +1,7 @@
-/*jshint node:true*/
-
-var testInfo = require('ember-cli-test-info');
-
 module.exports = {
   description: 'Generates a view unit test.',
-  locals: function(options) {
-    return {
-      friendlyTestDescription: testInfo.description(options.entity.name, "Unit", "View")
-    };
-  },
+  locals: function() {
+    var blueprint = this.lookupBlueprint('view-test');
+    return blueprint.locals.apply(blueprint, arguments);
+  }
 };
